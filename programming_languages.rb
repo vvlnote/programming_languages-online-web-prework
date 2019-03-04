1980c52b8847194955e9aef7cc346cff134ea57c
Joy Lin <vvlnote@hotmail.com>
2019-03-04 19:20:05 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,13 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_data|
     language_data.each do |language, type|
-      if new_hash.has_key
+      if new_hash.has_key?(language) == false
+        new_hash[language] = type
+        new_hash[language][:style] = [style]
+      else
+        new_hash[language][:style] << style
+      end
     end
   end
+  new_hash
 end
